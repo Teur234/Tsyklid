ca678864687e9ea449133ed8222752c5ab6f02f3
Opilane TTHK <opilane@tthk.ee>
2025-11-10 10:33:56 +0200

    tsüklid

--- DoWhile/DoWhile/Program.cs
+++ DoWhile/DoWhile/Program.cs
@@ -1,34 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+Console.WriteLine("Mis on su lemmikvärvid? sisesta ükshaaval \nKui rohkem värve ei ole, kirjuta \"rohkem pole\"");
+List<string> lemmikvärvid = new List<string>();
 
-string kasutajaNimi = "";
+string sisestus = "";
 do
 {
-Console.WriteLine("Palun sisesta oma kasutajanimi");
-kasutajaNimi = Console.ReadLine();
-}
-while (kasutajaNimi != "user1");
-if (kasutajaNimi == "user1")
+    Console.WriteLine("Sisesta 1 värv korraga: ");
+    sisestus = Console.ReadLine();
+    if (sisestus != "rohkem pole")
+    {
+        lemmikvärvid.Add(sisestus);
+    }
+
+} while (sisestus != "rohkem pole");
+
+foreach (var värv in lemmikvärvid)
 {
-    int ruuduSuurus = 0;
-    
-    do
+    switch (värv)
     {
-        Console.WriteLine("Kui suurt ruutu tahad?");
-        ruuduSuurus = int.Parse(Console.ReadLine());
-    } while (ruuduSuurus < 0 && ruuduSuurus > 20);
-    char reaKujund = ' ';
-    string üksRida = "";
-    int tsükliMuutuja = ruuduSuurus;
-    do
-    {
-        üksRida += "_"+reaKujund;
-        tsükliMuutuja -= 1;
-    } while (tsükliMuutuja != 0);
-    tsükliMuutuja = ruuduSuurus;
-    do
-    {
-        Console.WriteLine(üksRida);
-        tsükliMuutuja -= 1;
-    } while (tsükliMuutuja != 0);
-    Console.WriteLine("$palun, siin on sinu ruut, suurusega{ruuduSuurus}x{ruuduSuurus}");
+        case "punane":
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("p u n a n e");
+            break;
+        case "oranž":
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Kahjuks ei ole oranz saadaval");
+            break;
+        case "kollane":
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" k o l l a n e");
+            break;
+        case "roheline":
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("r o h e l i n e");
+            break;
+        case "lilla":
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("l i l l a");
+            break;
+        case "valge":
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("v a l g e");
+            break;
+        case "must ":
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("m u s t ");
+            break;
+        case "pruun":
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("p r u u n ");
+            break;
+        case "hall":
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("h a l l ");
+            break;
+        default:
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("$ Sellist värvi ei tunne {värv}");
+            break;
+    }
 }
+
