9de781925056af73ef05fb6aec44c82b0abe26ff
Opilane TTHK <opilane@tthk.ee>
2025-11-24 09:37:11 +0200

    aaa

---
+++ Castimine/Castimine/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Castimine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- Meetod 2/Meetod 2/Program.cs
+++ Meetod 2/Meetod 2/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Tervitus();
+            string kasutajanimi = "";
+            kasutajanimi = NimeKüsimine(kasutajanimi);
+
+            Console.WriteLine("Mis on sinu nädalane eelarve");
+            float eelarve = 0.00f;
+            bool kaskasuatajanõustub = false;
+            while (kaskasuatajanõustub != true)
+            {
+                Console.WriteLine("$ Praegune eelarve on {eelarve}, kas see on õige");
+                Console.WriteLine("Vaata kas jah või ei:");
+                string mistaütles = Console.ReadLine();
+
+                if (mistaütles == "jah")
+                {
+                    kaskasuatajanõustub = true;
+                }
+
+            }
+            Console.WriteLine("${kasutajanimi}sinu eelarve on{sinueelarvepäevas(eelarve)};");
+
+        }
+
+        private static string NimeKüsimine(string kasutajanimi)
+        {
+            while (kasutajanimi == "")
+            {
+                Tervitus();
+                Console.WriteLine("Palun sisesta oma kasutajanimi");
+                kasutajanimi = Console.ReadLine();
+
+            }
+
+            return kasutajanimi;
+        }
+
+        public static void Tervitus()
+        {
+            Console.WriteLine("ommik!");
+        }
+        public static int sinueelarvepäevas(float eelarve, string kasuatajanimi)
+        {
+            int sinueelarvepäevas = 7;
+            return sinueelarvepäevas;
         }
     }
 }
